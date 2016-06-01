1bdc43fe1f1ef672f25755b70eece44060a19f9f
Stephen Hines <srhines@google.com>
2016-06-01 02:26:40 -0700

    Add support for building compiler-rt.
    Change-Id: Ib576c0f70e48349065524604c17cc60028790763

--- android/build.py
+++ android/build.py
@@ -57,13 +57,12 @@ def rm_cmake_cache(dir):
             rm_tree(os.path.join(dirpath, 'CMakeFiles'))
 
 
-def build_llvm(out_path, defines, env):
+def build_llvm(out_path, defines, env, cmake_path, target=None):
     flags = ['-G', 'Ninja']
     for key in defines:
         newdef = '-D' + key + '=' + defines[key]
         flags += [newdef]
-    llvm_root_path = android_path('llvm')
-    flags += [llvm_root_path]
+    flags += [cmake_path]
 
     check_create_path(out_path)
     os.chdir(out_path)
@@ -71,12 +70,96 @@ def build_llvm(out_path, defines, env):
     # due to the updated cmake generated files.
     #rm_cmake_cache(out_path)
 
+    if target:
+        ninja_target = [target]
+    else:
+        ninja_target = []
+
+    print subprocess.list2cmdline(flags)
     subprocess.check_call(
         ['cmake'] + flags, cwd=out_path, env=env)
     subprocess.check_call(
-        ['ninja'], cwd=out_path, env=env)
+        ['ninja'] + ninja_target, cwd=out_path, env=env)
     subprocess.check_call(
         ['ninja', 'install'], cwd=out_path, env=env)
+
+
+def build_crts(base_cmake_defines, stage2_install):
+    cc = os.path.join(stage2_install, 'bin', 'clang')
+    cxx = os.path.join(stage2_install, 'bin', 'clang++')
+    llvm_config = os.path.join(stage2_install, 'bin', 'llvm-config')
+
+    crt_configs = [
+        ('arm', 'arm', 'arm/arm-linux-androideabi-4.9/arm-linux-androideabi', 'arm-linux-androideabi', ''),
+        ('arm64', 'aarch64', 'aarch64/aarch64-linux-android-4.9/aarch64-linux-android', 'aarch64-linux-android', ''),
+        # Disable x86 due to http://b/29371418.
+        #('x86', 'i686', 'x86/x86_64-linux-android-4.9/x86_64-linux-android', 'i686-linux-android', '-m32'),
+        #('x86_64', 'x86_64', 'x86/x86_64-linux-android-4.9/x86_64-linux-android', 'x86_64-linux-android', '-m64'),
+        # Disable mips32 because it doesn't even build.
+        #('mips', 'mipsel', 'mips/mips64el-linux-android-4.9/mips64el-linux-android', 'mipsel-linux-android', '-m32'),
+        ('mips64', 'mips64el', 'mips/mips64el-linux-android-4.9', 'mips64el-linux-android', '-m64')]
+
+    # Now build compiler-rt for each arch
+    for (target_arch, llvm_arch, toolchain_name, triple, extra_flags) in crt_configs:
+        print "Building compiler-rt for %s" % target_arch
+        crt_path = android_path('out', 'clangrt-' + target_arch)
+        crt_install = stage2_install
+
+        toolchain_dir = android_path('prebuilts/gcc/linux-x86/' + toolchain_name)
+        toolchain_bin = os.path.join(toolchain_dir, 'bin')
+        toolchain_usr = os.path.join(toolchain_dir, 'usr')
+        toolchain_usr_lib = os.path.join(toolchain_dir, 'usr', 'lib')
+        toolchain_lib = os.path.join(toolchain_dir, 'lib')
+        toolchain_lib_triple = os.path.join(toolchain_dir, 'lib', triple)
+        toolchain_usr_lib_triple = os.path.join(toolchain_dir, 'usr', 'lib', triple)
+        sysroot = android_path('prebuilts/ndk/current/platforms/android-24/arch-' +
+                target_arch)
+
+        cflags = ['-target %s' % triple,
+                  '--sysroot=%s' % sysroot,
+                  '-B%s' % toolchain_dir,
+                  extra_flags,
+                 ]
+        cxxflags = []
+        ldflags = ['-L%s' % toolchain_usr_lib,
+                   '-L%s' % toolchain_lib,
+                   '-L%s' % toolchain_lib_triple,
+                   '-L%s' % toolchain_usr_lib_triple,
+                   extra_flags,
+                  ]
+
+        crt_defines = base_cmake_defines.copy()
+        crt_defines['LLVM_BUILD_EXTERNAL_COMPILER_RT'] = 'ON'
+        crt_defines['COMPILER_RT_DEFAULT_TARGET_ARCH'] = llvm_arch
+        crt_defines['LLVM_TARGET_ARCH'] = llvm_arch
+        crt_defines['LLVM_TARGETS_TO_BUILD'] = llvm_arch
+        crt_defines['LLVM_CONFIG_PATH'] = llvm_config
+        crt_defines['CMAKE_INSTALL_PREFIX'] = crt_install
+        crt_defines['CMAKE_C_COMPILER'] = cc
+        crt_defines['CMAKE_CXX_COMPILER'] = cxx
+        crt_defines['CMAKE_C_FLAGS'] = ' '.join(cflags)
+        crt_defines['CMAKE_CXX_FLAGS'] = ' '.join(cxxflags)
+        crt_defines['CMAKE_EXE_LINKER_FLAGS'] = ' '.join(ldflags)
+        crt_defines['CMAKE_SHARED_LINKER_FLAGS'] = ' '.join(ldflags)
+        crt_defines['CMAKE_MODULE_LINKER_FLAGS'] = ' '.join(ldflags)
+        crt_defines['CMAKE_CROSSCOMPILING'] = 'TRUE'
+        crt_defines['CMAKE_LIBRARY_PATH'] = toolchain_usr
+        crt_defines['CMAKE_LIBRARY_ARCHITECTURE'] = triple
+        crt_defines['CMAKE_C_COMPILER_WORKS'] = 'ON'
+        crt_defines['CMAKE_CXX_COMPILER_WORKS'] = 'ON'
+        crt_defines['COMPILER_RT_STANDALONE_BUILD'] = 'ON'
+        crt_defines['COMPILER_RT_DEFAULT_TARGET_TRIPLE'] = triple
+
+        crt_defines['CMAKE_SYSTEM_NAME'] = 'Linux'
+        crt_defines['CMAKE_ASM_FLAGS'] = "-target %s %s" % (triple, extra_flags)
+
+        crt_env = dict(ORIG_ENV)
+
+        crt_cmake_path = os.path.join(android_path('llvm'), 'projects',
+                'compiler-rt', 'lib', 'builtins')
+        rm_cmake_cache(crt_path)
+        build_llvm(out_path=crt_path, defines=crt_defines, env=crt_env,
+                cmake_path=crt_cmake_path)
 
 
 def main():
@@ -104,12 +187,14 @@ def main():
     stage1_defines['CMAKE_CXX_FLAGS'] = ' '.join(cxxflags)
     stage1_defines['CMAKE_EXE_LINKER_FLAGS'] = ' '.join(ldflags)
     stage1_defines['CMAKE_SHARED_LINKER_FLAGS'] = ' '.join(ldflags)
+    stage1_defines['CMAKE_DYNAMIC_LINKER_FLAGS'] = ' '.join(ldflags)
     stage1_defines['CMAKE_MODULE_LINKER_FLAGS'] = ' '.join(ldflags)
 
     stage1_env = dict(ORIG_ENV)
 
     # Build/install the stage 1 toolchain
-    build_llvm(out_path=stage1_path, defines=stage1_defines, env=stage1_env)
+    build_llvm(out_path=stage1_path, defines=stage1_defines, env=stage1_env,
+            cmake_path=android_path('llvm'))
 
     # TODO(srhines): Build LTO plugin (Chromium folks say ~10% perf speedup)
 
@@ -132,7 +217,10 @@ def main():
 
     stage2_env = dict(ORIG_ENV)
 
-    build_llvm(out_path=stage2_path, defines=stage2_defines, env=stage2_env)
+    build_llvm(out_path=stage2_path, defines=stage2_defines, env=stage2_env,
+            cmake_path=android_path('llvm'))
+
+    build_crts(base_cmake_defines, stage2_install)
 
     return 0
 
