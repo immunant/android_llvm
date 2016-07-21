1f0c8af33af99dbba1606efd023e49933246fa9e
Stephen Hines <srhines@google.com>
2016-07-21 08:49:29 -0700

    Set a better default triple for the darwin host compiler.
    Bug: http://b/19787571
    
    Note that this patch doesn't alter other paths/options on darwin, so it
    might still be necessary to pass additional flags to make it useful.
    
    Change-Id: I423fe5765e82a721c7839817537281b1458b6068

--- host/include/llvm/Config/llvm-config.h
+++ host/include/llvm/Config/llvm-config.h
@@ -27,7 +27,11 @@
 #define LLVM_DATADIR "/opt/llvm-android/share/llvm"
 
 /* Target triple LLVM will generate code for by default */
+#if defined(__APPLE__)
+#define LLVM_DEFAULT_TARGET_TRIPLE "x86_64-apple-darwin"
+#else
 #define LLVM_DEFAULT_TARGET_TRIPLE "x86_64-unknown-linux"
+#endif
 
 /* Installation directory for documentation */
 #define LLVM_DOCSDIR "/opt/llvm-android/share/doc/llvm"
