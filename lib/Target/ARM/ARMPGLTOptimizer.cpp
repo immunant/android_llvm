d6eed0972ea20d20fe6f51b1b8c05cb0179f4e44
Stephen Crane <sjc@immunant.com>
2017-04-19 15:38:59 -0700

    Delete unused constant pool entries after optimizing intra-page calls

--- lib/Target/ARM/ARMPGLTOptimizer.cpp
+++ lib/Target/ARM/ARMPGLTOptimizer.cpp
@@ -48,6 +48,7 @@ namespace {
     
     bool isSameBin(const GlobalValue *GV);
     void replacePGLTUses(SmallVectorImpl<int> &CPEntries);
+    void deleteOldCPEntries(SmallVectorImpl<int> &CPEntries);
   };
   char ARMPGLTOpt::ID = 0;
 }
@@ -66,8 +67,7 @@ bool ARMPGLTOpt::runOnMachineFunction(MachineFunction &Fn) {
   ConstantPool = Fn.getConstantPool();
   isThumb2 = Fn.getInfo<ARMFunctionInfo>()->isThumb2Function();
 
-  SmallVector<int, 8> PGLTOffCPEntries;
-  SmallVector<int, 8> BinOffCPEntries;
+  SmallVector<int, 8> PGLTCPEntries;
 
   auto &CPEntries = ConstantPool->getConstants();
 
@@ -80,23 +80,24 @@ bool ARMPGLTOpt::runOnMachineFunction(MachineFunction &Fn) {
 
     ARMConstantPoolValue *ACPV =
       static_cast<ARMConstantPoolValue*>(Entry.Val.MachineCPVal);
-    if (ACPV->getModifier() == ARMCP::PGLTOFF) {
+    if (ACPV->getModifier() == ARMCP::PGLTOFF ||
+        ACPV->getModifier() == ARMCP::BINOFF) {
       const GlobalValue *GV = cast<ARMConstantPoolConstant>(ACPV)->getGV();
       if (isSameBin(GV)) 
-        PGLTOffCPEntries.push_back(i);
-    } else if (ACPV->getModifier() == ARMCP::BINOFF) {
-      const GlobalValue *GV = cast<ARMConstantPoolConstant>(ACPV)->getGV();
-      if (isSameBin(GV)) 
-        BinOffCPEntries.push_back(i);
+        PGLTCPEntries.push_back(i);
     }
   }
 
+  if (PGLTCPEntries.empty())
+    return false;
+
   // Replace users of PGLT-indirect CP entries with direct calls
-  replacePGLTUses(PGLTOffCPEntries);
+  replacePGLTUses(PGLTCPEntries);
 
   // Delete unneeded CP entries
+  deleteOldCPEntries(PGLTCPEntries);
 
-  return !PGLTOffCPEntries.empty();
+  return true;
 }
 
 void ARMPGLTOpt::replacePGLTUses(SmallVectorImpl<int> &CPEntries) {
@@ -127,16 +128,27 @@ void ARMPGLTOpt::replacePGLTUses(SmallVectorImpl<int> &CPEntries) {
     MachineInstr *MI = I.first;
     const GlobalValue *GV = I.second;
 
-    unsigned DestReg = MI->getOperand(0).getReg();
     SmallVector<MachineInstr*, 4> InstrQueue;
-    for (auto &User : MRI.use_instructions(DestReg))
-      InstrQueue.push_back(&User);
+    InstrQueue.push_back(MI);
 
     while (!InstrQueue.empty()) {
       MachineInstr *User = InstrQueue.back();
       InstrQueue.pop_back();
       if (User->isCall()) {
-        unsigned CallOpc = isThumb2 ? ARM::tBL : ARM::BL;
+        unsigned CallOpc;
+        switch (User->getOpcode()) {
+        case ARM::TCRETURNri:
+          CallOpc = ARM::TCRETURNdi;
+          break;
+        case ARM::BLX:
+          CallOpc = ARM::BL;
+          break;
+        case ARM::tBLXr:
+          CallOpc = ARM::tBL;
+          break;
+        default:
+          llvm_unreachable("Unhandled ARM call opcode.");
+        }
         MachineInstrBuilder MIB = BuildMI(*User->getParent(), *User,
                                           User->getDebugLoc(), TII->get(CallOpc));
         if (isThumb2)
@@ -144,13 +156,57 @@ void ARMPGLTOpt::replacePGLTUses(SmallVectorImpl<int> &CPEntries) {
         MIB.addGlobalAddress(GV, 0, 0);
         for (int i = 1, e = User->getNumOperands(); i < e; ++i)
           MIB.add(User->getOperand(i));
+        User->eraseFromParent();
       } else {
         for (auto Op : User->defs()) {
           for (auto &User : MRI.use_instructions(Op.getReg()))
             InstrQueue.push_back(&User);
         }
+        User->eraseFromParent();
       }
     }
+  }
+}
+
+void ARMPGLTOpt::deleteOldCPEntries(SmallVectorImpl<int> &CPEntries) {
+  std::sort(CPEntries.begin(), CPEntries.end());
+  std::vector<int> IndexMapping;
+  int NewI = 0;
+  for (int i = 0, e = ConstantPool->getConstants().size(); i < e; ++i) {
+    bool deleted = false;
+    for (auto DeleteI : CPEntries) {
+      if (i == DeleteI) {
+        deleted = true;
+        break;
+      }
+    }
+    if (deleted)
+      IndexMapping.push_back(-1);
+    else
+      IndexMapping.push_back(NewI++);
+  }
+
+  for (int i = 0, e = IndexMapping.size(); i < e; ++i) {
+    DEBUG(dbgs() << "Index mapping " << i << " -> " << IndexMapping[i] << '\n');
+  }
+
+  for (auto &BB : *MF) {
+    for (auto &MI : BB) {
+      for (auto &Op : MI.explicit_uses())
+        if (Op.isCPI()) {
+          int CPIndex = Op.getIndex();
+          assert (IndexMapping[CPIndex] != -1 &&
+                  "We should already have deleted this constant pool use");
+
+          Op.setIndex(IndexMapping[CPIndex]);
+        }
+    }
+  }
+
+  // Iterate entries to delete in reverse order so each deletion will not affect
+  // the index of future deletions.
+  for (auto I = CPEntries.rbegin(), E = CPEntries.rend(); I != E; ++I) {
+    ConstantPool->eraseIndex(*I);
   }
 }
 
