6ec4f170f599f08dab263554f5b81681d677ca2e
Pirama Arumuga Nainar <pirama@google.com>
2017-06-13 17:09:55 -0700

    Specify a proper prebuilt compiler to construct our stage-1 compiler.
    Bug: http://b/28196329
    Test: Ran toolchain/llvm/android/build.py on Linux and Darwin.
    
    Change-Id: Ib7df7eacedb2fa38dbbb7b7b10fb23d263e161cd

--- android/build.py
+++ android/build.py
@@ -53,6 +53,23 @@ def build_os_type():
         return 'darwin-x86'
 
 
+# This is the baseline stable version of Clang to start our stage-1 build.
+def clang_prebuilt_version():
+    return 'clang-4053586'
+
+
+def clang_prebuilt_base_dir():
+    return utils.android_path('prebuilts/clang/host', build_os_type(),
+                              clang_prebuilt_version())
+
+def clang_prebuilt_bin_dir():
+    return utils.android_path(clang_prebuilt_base_dir(), 'bin')
+
+
+def clang_prebuilt_lib_dir():
+    return utils.android_path(clang_prebuilt_base_dir(), 'lib64')
+
+
 def cmake_prebuilt_bin_dir():
     return utils.android_path('prebuilts/cmake', build_os_type(), 'bin')
 
@@ -294,7 +311,8 @@ def build_libfuzzers(stage2_install, clang_version):
             shutil.copy2(os.path.join(header_src, f), header_dst)
 
 
-def build_llvm(targets, build_dir, install_dir, extra_defines=None):
+def build_llvm(targets, build_dir, install_dir, extra_defines=None,
+               extra_env=None):
     cmake_defines = base_cmake_defines()
     cmake_defines['CMAKE_INSTALL_PREFIX'] = install_dir
     cmake_defines['LLVM_TARGETS_TO_BUILD'] = targets
@@ -304,6 +322,8 @@ def build_llvm(targets, build_dir, install_dir, extra_defines=None):
         cmake_defines.update(extra_defines)
 
     env = dict(ORIG_ENV)
+    if extra_env is not None:
+        env.update(extra_env)
 
     invoke_cmake(out_path=build_dir, defines=cmake_defines, env=env,
                  cmake_path=utils.llvm_path())
@@ -369,11 +389,41 @@ def build_stage1():
 
     stage1_extra_defines = dict()
     stage1_extra_defines['LLVM_BUILD_TOOLS'] = 'OFF'
-    stage1_extra_defines['LLVM_BUILD_RUNTIME'] = 'OFF'
+    stage1_extra_defines['LLVM_BUILD_RUNTIME'] = 'ON'
     stage1_extra_defines['CLANG_ENABLE_ARCMT'] = 'OFF'
     stage1_extra_defines['CLANG_ENABLE_STATIC_ANALYZER'] = 'OFF'
+    stage1_extra_defines['CMAKE_C_COMPILER'] = os.path.join(
+            clang_prebuilt_bin_dir(), 'clang')
+    stage1_extra_defines['CMAKE_CXX_COMPILER'] = os.path.join(
+            clang_prebuilt_bin_dir(), 'clang++')
     stage1_extra_defines['LLVM_TOOL_CLANG_TOOLS_EXTRA_BUILD'] = 'OFF'
     stage1_extra_defines['LLVM_TOOL_OPENMP_BUILD'] = 'OFF'
+
+    # Have clang use libc++, ...
+    stage1_extra_defines['LLVM_ENABLE_LIBCXX'] = 'ON'
+
+    # ... and point CMake to the libc++.so from the prebuilts.  Install an rpath
+    # to prevent linking with the newly-built libc++.so
+    ldflags = ['-Wl,-rpath,' + clang_prebuilt_lib_dir()]
+    stage1_extra_defines['CMAKE_EXE_LINKER_FLAGS'] = ' '.join(ldflags)
+    stage1_extra_defines['CMAKE_SHARED_LINKER_FLAGS'] = ' '.join(ldflags)
+    stage1_extra_defines['CMAKE_MODULE_LINKER_FLAGS'] = ' '.join(ldflags)
+
+    # Make libc++.so a symlink to libc++.so.x instead of a linker script that
+    # also adds -lc++abi.  Statically link libc++abi to libc++ so it is not
+    # necessary to pass -lc++abi explicitly.  This is needed only for Linux.
+    if build_os_type() == 'linux-x86':
+        stage1_extra_defines['LIBCXX_ENABLE_ABI_LINKER_SCRIPT'] = 'OFF'
+        stage1_extra_defines['LIBCXX_ENABLE_STATIC_ABI_LIBRARY'] = 'ON'
+
+    # Do not build compiler-rt for Darwin.  We don't ship host (or any
+    # prebuilt) runtimes for Darwin anyway.  Attempting to build these will
+    # fail compilation of lib/builtins/atomic_*.c that only get built for
+    # Darwin and fail compilation due to us using the bionic version of
+    # stdatomic.h.
+    if build_os_type() == 'darwin-x86':
+          stage1_extra_defines['LLVM_BUILD_EXTERNAL_COMPILER_RT'] = 'ON'
+
     build_llvm(targets=stage1_targets, build_dir=stage1_path,
                install_dir=stage1_install, extra_defines=stage1_extra_defines)
     return stage1_install
@@ -391,9 +441,36 @@ def build_stage2(stage1_install, stage2_targets):
     stage2_extra_defines = dict()
     stage2_extra_defines['CMAKE_C_COMPILER'] = stage2_cc
     stage2_extra_defines['CMAKE_CXX_COMPILER'] = stage2_cxx
+    stage2_extra_defines['LLVM_BUILD_RUNTIME'] = 'ON'
+    stage2_extra_defines['LLVM_ENABLE_LIBCXX'] = 'ON'
+    stage2_extra_defines['LIBCXX_ENABLE_STATIC_ABI_LIBRARY'] = 'ON'
+    stage2_extra_defines['LIBCXX_ENABLE_ABI_LINKER_SCRIPT'] = 'OFF'
+
+    # Make libc++.so a symlink to libc++.so.x instead of a linker script that
+    # also adds -lc++abi.  Statically link libc++abi to libc++ so it is not
+    # necessary to pass -lc++abi explicitly.  This is needed only for Linux.
+    if build_os_type() == 'linux-x86':
+        stage2_extra_defines['LIBCXX_ENABLE_STATIC_ABI_LIBRARY'] = 'ON'
+        stage2_extra_defines['LIBCXX_ENABLE_ABI_LINKER_SCRIPT'] = 'OFF'
+
+    # Do not build compiler-rt for Darwin.  We don't ship host (or any
+    # prebuilt) runtimes for Darwin anyway.  Attempting to build these will
+    # fail compilation of lib/builtins/atomic_*.c that only get built for
+    # Darwin and fail compilation due to us using the bionic version of
+    # stdatomic.h.
+    if build_os_type == 'darwin-x86':
+        stage2_extra_defines['LLVM_BUILD_EXTERNAL_COMPILER_RT'] = 'ON'
+
+    # Point CMake to the libc++ from stage1.  It is possible that once built,
+    # the newly-built libc++ may override this because of the rpath pointing to
+    # $ORIGIN/../lib64.  That'd be fine because both libraries are built from
+    # the same sources.
+    stage2_extra_env = dict()
+    stage2_extra_env['LD_LIBRARY_PATH'] = os.path.join(stage1_install, 'lib64')
 
     build_llvm(targets=stage2_targets, build_dir=stage2_path,
-               install_dir=stage2_install, extra_defines=stage2_extra_defines)
+               install_dir=stage2_install, extra_defines=stage2_extra_defines,
+               extra_env=stage2_extra_env)
     return stage2_install
 
 
