0bc8f24616331eb0fc81a5d75c2f3da0c4b604c8
Julian Lettner <julian.lettner@immunant.com>
2017-08-09 11:47:04 -0700

    Remove commented out code
    Change-Id: I3f0a9df9f5d24fbb7226766fb0c65e7fee11425b

--- lib/Target/AArch64/AArch64PagerandoOptimizer.cpp
+++ lib/Target/AArch64/AArch64PagerandoOptimizer.cpp
@@ -13,9 +13,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "AArch64.h"
-//#include "ARMBaseInstrInfo.h"
-//#include "ARMBaseRegisterInfo.h"
-//#include "ARMConstantPoolValue.h"
 #include "AArch64MachineFunctionInfo.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
@@ -43,8 +40,6 @@ public:
 private:
   void optimizeCall(MachineInstr *MI, const Function *Callee);
   void replaceWithDirectCall(MachineInstr *MI, const Function *Callee);
-//  void replaceWithPCRelativeCall(MachineInstr *MI, const Function *Callee);
-//  void deleteCPEntries(MachineFunction &MF, const SmallSet<int, 8> &Workset);
 };
 } // end anonymous namespace
 
@@ -55,33 +50,6 @@ INITIALIZE_PASS(AArch64PagerandoOptimizer, "pagerando-optimizer-aarch64",
 FunctionPass *llvm::createAArch64PagerandoOptimizerPass() {
   return new AArch64PagerandoOptimizer();
 }
-
-//static bool isIntraBin(const MachineConstantPoolEntry &E, StringRef BinPrefix) {
-//  if (!E.isMachineConstantPoolEntry()) return false;
-//
-//  // ARMConstantPoolValue lacks casting infrastructure to use dyn_cast directly
-//  auto *CPV = static_cast<ARMConstantPoolValue*>(E.Val.MachineCPVal);
-//  auto *CPC = dyn_cast<ARMConstantPoolConstant>(CPV);
-//  if (!CPC) return false;
-//
-//  auto M = CPC->getModifier();
-//  auto *F = dyn_cast_or_null<Function>(CPC->getGV());
-//
-//  return (M == ARMCP::POTOFF || M == ARMCP::BINOFF)
-//      && F && F->getSectionPrefix() == BinPrefix;
-//}
-//
-//static const Function *getCallee(const MachineConstantPoolEntry &E) {
-//  auto *CPC = static_cast<ARMConstantPoolConstant*>(E.Val.MachineCPVal);
-//  return cast<Function>(CPC->getGV());
-//}
-//
-//static int getCPIndex(const MachineInstr &MI) {
-//  if (MI.mayLoad() && MI.getNumOperands() > 1 && MI.getOperand(1).isCPI()) {
-//    return MI.getOperand(1).getIndex();
-//  }
-//  return -1;
-//}
 
 static const Function *getCallee(const MachineInstr &MI) {
   return cast<Function>(MI.getOperand(2).getGlobal());
@@ -119,21 +87,11 @@ bool AArch64PagerandoOptimizer::runOnMachineFunction(MachineFunction &MF) {
   // Optimize intra-bin calls
   for (auto *MI : Worklist) {
     auto *Callee = getCallee(*MI);
-    errs() << "before optimze:\n";
-    MI->getParent()->dump();
     optimizeCall(MI, Callee);
-    errs() << "AFTER optimze:\n";
-    MI->getParent()->dump();
   }
-//
-//  deleteCPEntries(MF, Workset);
-//
+
   return true;
 }
-//
-//static bool isBXCall(unsigned Opc) {
-//  return Opc == ARM::BX_CALL || Opc == ARM::tBX_CALL;
-//}
 
 void AArch64PagerandoOptimizer::optimizeCall(MachineInstr *MI,
                                              const Function *Callee) {
@@ -151,9 +109,7 @@ void AArch64PagerandoOptimizer::optimizeCall(MachineInstr *MI,
         }
       }
       MI->eraseFromParent();
-//    } else if (isBXCall(MI->getOpcode())) {
-//      replaceWithPCRelativeCall(MI, Callee);
-    } else { // Standard indirect call
+    } else {
       replaceWithDirectCall(MI, Callee);
     }
   }
@@ -180,13 +136,6 @@ void AArch64PagerandoOptimizer::replaceWithDirectCall(MachineInstr *MI,
       .addGlobalAddress(Callee);
 
   int SkipOps = 1;
-//  if (MI->getOpcode() == ARM::tBLXr) { // Short instruction
-//    auto CondOp = predOps(ARMCC::AL);
-//    MIB.add(CondOp);
-//    SkipOps += CondOp.size();
-//  }
-//  MIB.addGlobalAddress(Callee);
-//
   // Copy over remaining operands
   auto RemainingOps = make_range(MI->operands_begin() + SkipOps,
                                  MI->operands_end());
@@ -196,80 +145,3 @@ void AArch64PagerandoOptimizer::replaceWithDirectCall(MachineInstr *MI,
 
   MI->eraseFromParent();
 }
-
-//// Replace indirect register operand with more efficient PC-relative access
-//void AArch64PagerandoOptimizer::replaceWithPCRelativeCall(MachineInstr *MI,
-//                                                   const Function *Callee) {
-//  auto &MBB = *MI->getParent();
-//  auto &MF = *MBB.getParent();
-//  auto &C = MF.getFunction()->getContext();
-//  auto &AFI = *MF.getInfo<ARMFunctionInfo>();
-//  auto &TII = *MF.getSubtarget().getInstrInfo();
-//  auto &TLI = *MF.getSubtarget().getTargetLowering();
-//  auto &DL = MF.getDataLayout();
-//  auto &MRI = MF.getRegInfo();
-//  auto isThumb = AFI.isThumbFunction();
-//
-//  // Create updated CP entry for callee
-//  auto Label = AFI.createPICLabelUId();
-//  auto PCAdj = isThumb ? 4 : 8;
-//  auto *CPV = ARMConstantPoolConstant::Create(
-//      Callee, Label, ARMCP::CPValue, PCAdj, ARMCP::no_modifier, false);
-//  auto Alignment = DL.getPrefTypeAlignment(Type::getInt32PtrTy(C));
-//  auto Index = MF.getConstantPool()->getConstantPoolIndex(CPV, Alignment);
-//
-//  // Load callee offset into register
-//  auto Opc = AFI.isThumb2Function() ? ARM::t2LDRpci : ARM::LDRcp;
-//  auto OffsetReg = MRI.createVirtualRegister(&ARM::rGPRRegClass);
-//  auto MIB = BuildMI(MBB, *MI, MI->getDebugLoc(), TII.get(Opc), OffsetReg)
-//      .addConstantPoolIndex(Index);
-//  if (Opc == ARM::LDRcp) MIB.addImm(0);
-//  MIB.add(predOps(ARMCC::AL));
-//
-//  // Compute callee address by adding PC
-//  // FIXME: this is ugly // comment by Stephen
-//  auto RegClass = TLI.getRegClassFor(TLI.getPointerTy(DL));
-//  auto AddressReg = MRI.createVirtualRegister(RegClass);
-//  Opc = isThumb ? ARM::tPICADD : ARM::PICADD;
-//  MIB = BuildMI(MBB, *MI, MI->getDebugLoc(), TII.get(Opc), AddressReg)
-//      .addReg(OffsetReg)
-//      .addImm(Label);
-//  if (!isThumb) MIB.add(predOps(ARMCC::AL));
-//
-//  // Replace register operand
-//  MI->getOperand(0).setReg(AddressReg);
-//}
-//
-//void AArch64PagerandoOptimizer::deleteCPEntries(MachineFunction &MF,
-//                                         const SmallSet<int, 8> &Workset) {
-//  auto *CP = MF.getConstantPool();
-//  int Size = CP->getConstants().size();
-//  int Indices[Size];
-//
-//  // Create CP index mapping: Indices[Old] -> New
-//  for (int Old = 0, New = 0; Old < Size; ++Old) {
-//    Indices[Old] = Workset.count(Old) ? -1 : New++;
-//  }
-//
-//  // Update remaining (inter-bin) CP references
-//  for (auto &BB : MF) {
-//    for (auto &MI : BB) {
-//      for (auto &Op : MI.explicit_uses()) {
-//        if (Op.isCPI()) {
-//          int Old = Op.getIndex();
-//          int New = Indices[Old];
-//          assert (New != -1 && "CP entry use should have been deleted");
-//          Op.setIndex(New);
-//        }
-//      }
-//    }
-//  }
-//
-//  // Delete now unreferenced (intra-bin) CP entries (in reverse order so
-//  // deletion does not affect the index of future deletions)
-//  for (int Old = Size - 1; Old >= 0; --Old) {
-//    if (Indices[Old] == -1) {
-//      CP->eraseIndex(Old);
-//    }
-//  }
-//}
